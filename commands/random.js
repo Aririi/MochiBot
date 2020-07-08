c3673077d319a7a888f7baff499116ffc467dece
Aririi <37724510+Aririi@users.noreply.github.com>
2020-07-07 22:33:26 -0700

    more tweaks part 2, minor fixes

--- commands/random.js
+++ commands/random.js
@@ -5,13 +5,11 @@ const { randomColor, name } = require('../config.json');
 module.exports = {
 	name: 'random',
 	description: 'Uses a few APIs to get random media of the argument given (see usage list).',
-	usage: '[avatar/icon/pfp, cat/meow, dog/wood foxgirl, goose, gecg, kemonomimi, neko/nyan, nekogif/nyangif, waifu]',
-	devOnly: true / false,
+	usage: '[avatar/icon/pfp, cat/meow, dog/woof, foxgirl, goose, gecg, kemonomimi, neko/nyan, nekogif/nyangif, waifu]',
 	execute(message, args) {
 		// required to init nekos.life wrapper
 		const nekoLifeClient = require('nekos.life');
 		const nekolife = new nekoLifeClient;
-
 
 		// async functions to be executed based on the argument given
 		async function randomAvatar() {
@@ -72,31 +70,43 @@ module.exports = {
 		// }
 
 
-		// logical checks for what arg was passed, executing async funcs accordingly
-		if (args[0] === 'avatar' || args[0] === 'icon' || args[0] === 'pfp') {randomAvatar();}
-		else if (args[0] === 'cat' || args[0] === 'meow') {randomCat();}
-		else if (args[0] === 'dog' || args[0] === 'woof') {randomDog();}
-		else if (args[0] === 'goose' || args[0] === 'geese') {randomGoose();}
-		else if (args[0] === 'foxgirl') {randomFoxgirl();}
-		else if (args[0] === 'gecg' || args[0] === 'gmo') {randomGECG();}
-		else if (args[0] === 'holo') {randomHolo();}
-		else if (args[0] === 'kemonomimi') {randomKemonomimi();}
-		else if (args[0] === 'lizard') {randomLizard();}
-		else if (args[0] === 'neko' || args[0] === 'nyan') {randomNeko();}
-		else if (args[0] === 'nekogif' || args[0] === 'nyangif') {randomNekoGIF();}
-		else if (args[0] === 'waifu') {randomWaifu();}
-		else if (args[0] === 'wallpaper') {randomWallpaper();}
-		else {
-			message.channel.send(`I don't know how to get "${args[0]}," maybe recommend it to the developer?`);
+		// loops if second argument contains number
+		if (isNaN(args[1]) === false) {
+			if (args[1] > 15) {return message.channel.send('That\'s too many.');}
+			else if (args[1] == 0) {return message.channel.send('Sending no random media.');}
+			else {
+				let loopCount = args[1];
+				while (loopCount > 0) {
+					setTimeout(randomTypeCheck, 1500);
+					loopCount = loopCount - 1;
+				}
+			}
 		}
 
-		function sendEmbed(description, result) {
+		function randomTypeCheck() {
+			// logical checks for what arg was passed, executing async funcs accordingly
+			if (args[0] === 'avatar' || args[0] === 'icon' || args[0] === 'pfp') {randomAvatar();}
+			else if (args[0] === 'cat' || args[0] === 'meow') {randomCat();}
+			else if (args[0] === 'dog' || args[0] === 'woof') {randomDog();}
+			else if (args[0] === 'goose' || args[0] === 'geese') {randomGoose();}
+			else if (args[0] === 'foxgirl') {randomFoxgirl();}
+			else if (args[0] === 'gecg' || args[0] === 'gmo') {randomGECG();}
+			else if (args[0] === 'holo') {randomHolo();}
+			else if (args[0] === 'kemonomimi') {randomKemonomimi();}
+			else if (args[0] === 'lizard') {randomLizard();}
+			else if (args[0] === 'neko' || args[0] === 'nyan') {randomNeko();}
+			else if (args[0] === 'nekogif' || args[0] === 'nyangif') {randomNekoGIF();}
+			else if (args[0] === 'waifu') {randomWaifu();}
+			else if (args[0] === 'wallpaper') {randomWallpaper();}
+			else {
+				message.channel.send(`I don't know how to get "${args[0]}," maybe recommend it to the developer?`);
+			}
+		}
+		function sendEmbed(randomColor, name, description, result) {
 			const randomMediaEmbed = new Discord.MessageEmbed(randomColor, name, description, result)
 				.setColor(`${randomColor}`)
 				.setTitle(`${name}'s Image Database`)
 				.setDescription(description)
-				// .attachFiles(['../MochiBot/media/MochiBot-64.png'])
-				// .setThumbnail('attachment://MochiBot-64.png')
 				.addFields({ name: 'Source:', value: result })
 				.setImage(result)
 				.setTimestamp()
@@ -104,6 +114,7 @@ module.exports = {
 					`${message.author.displayAvatarURL({ dynamic:true })}?size=32`);
 
 			message.channel.send(randomMediaEmbed);
+
 		}
 	},
 };
